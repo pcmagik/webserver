bae53812ce6be31c1f0c2d8a8e958d19ff12ef23
PC Magik - Home Lab <101038643+pcmagik@users.noreply.github.com>
2023-04-26 22:37:33 +0200

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,7 @@
 # Utwórz plik z rozszerzeniem .php (np. phpinfo.php) i wklej do zawartości tego pliku poniższy kod:
 <?php phpinfo(); ?>
+
+
+## jak użyć? Wpisz:
 # http://twojadomena.pl/phpinfo.php
+# https://twojadomena.pl/phpinfo.php
